712f562f912b35af4349942cf2d9783768a0912c
Khwla Akbar <52624784+khwla23@users.noreply.github.com>
2023-03-23 20:16:22 +0500

    Add files via upload

---
+++ Bytewise Fellowship/Task 3/src/Gallery.jsx
@@ -0,0 +1,20 @@
+function Profile() {
+    return (
+      <img
+        src="https://i.pinimg.com/564x/c9/3f/2e/c93f2e1c9cd6f7379662e7a91be31635.jpg"
+        alt="Spidy"
+      />
+    );
+  }
+  
+  export default function Gallery() {
+    return (
+      <section>
+        <h1>The Amazing Spider-Man</h1>
+        <Profile />
+        <Profile />
+        <Profile />
+      </section>
+    );
+  }
+  
